2ba2f45d9c8693e16cdf7d800a8f152b45abae4a
WEB DEVELOPER <buka4rill@gmail.com>
2020-05-03 16:18:59 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect Database
 connectDB();
@@ -13,12 +14,23 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 // Add route and send data
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKepper API' }));
+// app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKepper API' }));
 
 // Define Routes- Look at the routes files
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    // get anything that's not defined routes
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 
 
